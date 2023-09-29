09e685b530916f60a1470da042983f605df86bd8
Austin Williams <austinjwilliams141@gmail.com>
2023-09-29 14:04:36 -0400

    Added partial code for grid size slider

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const container = document.getElementById("main-container");
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+const gridSize = document.getElementById("slider");
+
+const container = document.getElementById("grid-container");
+for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
         const gridSquare = document.createElement("div");
         gridSquare.classList.add("square");
         container.appendChild(gridSquare);
     }
 }
+
+gridSize.addEventListener("input", function() {
+    const value = gridSize.value;
+    container.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+});
+
+//document.getElementById("grid-container").style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+
+/*document.querySelector("slider").addEventListener("change", function() {
+    document.getElementById("grid-container").style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+})*/
 
 function changeColor(event) {
     event.target.style.backgroundColor = "black";
